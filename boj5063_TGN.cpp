556c5d3d6e65e8c201398f8087d0325c4b1a1195
YEJI LEE <yagee97@naver.com>
2019-05-08 20:52:26 +0900

    구현
    구현

---
+++ boj5063_TGN.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		int r, e, c;
+		scanf("%d%d%d", &r, &e, &c);
+		if (r > e - c)
+			printf("do not advertise\n");
+		else if (r < e - c)
+			printf("advertise\n");
+		else
+			printf("does not matter\n");
+	}
+}
