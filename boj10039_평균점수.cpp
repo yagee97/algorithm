523c0ce043044bdf81c74e6d9704dbb9fc81207d
YEJI LEE <yagee97@naver.com>
2019-05-13 18:24:00 +0900

    구현
    구현

---
+++ boj10039_평균점수.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+	int sum = 0;
+	for (int i = 0; i < 5; i++) {
+		int score;
+		scanf("%d", &score);
+		if (score < 40)
+			score = 40;
+		sum += score;
+	}
+	
+	printf("%d", sum/5);
+}
