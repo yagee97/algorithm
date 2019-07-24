504bd47b27eb6c66fd25d23a0b7082d8f31238a9
YEJI LEE <yagee97@naver.com>
2019-07-24 18:31:04 +0900

    구간합_누적합
    구간합_누적합

---
+++ Algorithm concept/구간합_누적합.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+	int N, M, sum[100002];
+	scanf("%d %d", &N, &M);
+	sum[0] = 0;
+	int map[100002];
+	for (int i = 1; i <= N; i++) {
+		scanf("%d", &map[i]);
+		sum[i] = sum[i - 1] + map[i];
+	}
+	for (int i = 1; i <= M; i++) {
+		int num1, num2;
+		scanf("%d %d", &num1, &num2);
+		printf("%d\n", sum[num2] - sum[num1 - 1]);
+	}
+}
